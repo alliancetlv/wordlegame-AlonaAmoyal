229dc05fe1958b1607c6c77bd7f765e84d496d7f
AlonaAmoyal <alona.amoyal@gmail.com>
2024-10-25 14:51:56 +0300

    Update GameUI.java

--- src/GameUI.java
+++ src/GameUI.java
@@ -5,17 +5,14 @@ public class GameUI {
     private Scanner input;
 
     public GameUI(){
-        
+        input = new Scanner(System.in);
     }
 
     public String readUserGuess(){
-       this.input = in.next();
        return input;
     }
 
    public static void displayResult(int attemptsLeft, String guess, String feedback){
-      attemptsLeft = GameEngine.getAttemptsLeft();
-      feedback = GameEngine.evaluateGuess();
-      System.out.println("You have "+attemptsLeft+" attempts left.");
+      return "You have "+attemptsLeft+" attempts left. \n Your guess: "+guess+
    }
 }
