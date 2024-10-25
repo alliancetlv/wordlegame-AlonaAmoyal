6ba420b9671144ee829c269b0c2855466ad21457
AlonaAmoyal <alona.amoyal@gmail.com>
2024-10-25 13:13:50 +0300

    Update GameUI.java

--- src/GameUI.java
+++ src/GameUI.java
@@ -1,4 +1,12 @@
+import java.util.Scanner;
 
 public class GameUI {
-    
+   Scanner in = new Scanner(System.in);
+    private Scanner input;
+
+    public GameUI(){
+        this.input = in.next();
+    }
+
+    public 
 }
